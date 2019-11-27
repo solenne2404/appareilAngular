f89605070465c6148103eea504642784449441dd
sherve <sherve@norsys.fr>
2019-11-27 15:40:01 +0100

    add appareils array on appareils service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {AppareilService} from './services/appareil.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   isAuth = false;
-  
-  appareils = [
-    {
-      name: 'Machine à laver',
-      status: 'éteint'
-    },
-    {
-      name: 'Frigo',
-      status: 'allumé'
-    },
-    {
-      name: 'Ordinateur',
-      status: 'éteint'
-    }
-  ];
 
-  constructor() {
+  appareils: any[];
+
+  constructor(private appareilService: AppareilService) {
     setTimeout(
       () =>{
         this.isAuth = true;
       }, 4000
     );
+  }
+
+  ngOnInit() {
+    this.appareils = this.appareilService.appareils;
   }
 
   onAllumer(){
--- src/app/services/appareil.service.ts
+++ src/app/services/appareil.service.ts
@@ -1,5 +1,18 @@
 
 
 export class AppareilService {
-
+    appareils = [
+        {
+          name: 'Machine à laver',
+          status: 'éteint'
+        },
+        {
+          name: 'Frigo',
+          status: 'allumé'
+        },
+        {
+          name: 'Ordinateur',
+          status: 'éteint'
+        }
+      ];
 }
