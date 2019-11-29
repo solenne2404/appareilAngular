ac07835be70bc056432009c3dfd9baa3c225a0d8
sherve <sherve@norsys.fr>
2019-11-29 10:31:53 +0100

    add time counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, interval, Subscription } from 'rxjs';
+import 'rxjs'
 
 
 @Component({
@@ -8,14 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
-
+  secondes: number;
+  counterSubscription: Subscription;
 
   constructor() {
 
   }
 
   ngOnInit() {
-    
+    const counter = interval(1000);
+    this.counterSubscription = counter.subscribe(
+      (value: number) => {
+        this.secondes= value;
+      },
+      (error) => {
+        console.log('Uh-oh, an error occurred! : ' + error);
+      },
+      () => {
+        console.log('Observable complete!');
+      }
+    );
   }
 
+  ngOnDestroy() {
+    this.counterSubscription.unsubscribe();
+  }
 }
